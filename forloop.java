97947f76d27c684a9966a349710e89b777998644
PRERNA <147426811+Agarwalprerna@users.noreply.github.com>
2025-02-04 11:07:34 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,35 @@
+public class forloop {
+    public static void main(String args[]){
+
+        //first n odd numbers
+
+       /*  int n = 10;
+       for(int i = 0; i < n; i++)
+       {System.out.println(2*i + 1);}   */
+
+// first n even numbers(incrementing loop)
+  /*   int n = 10;
+    for(int i =0; i< n; i ++){
+        System.out.println(2*i);
+    } */
+
+ // decrement   
+
+/*  for(int a = 10; a!=0; a--){
+    System.out.println(a);
+ }  */
+
+//print  sum of first 20 odd numbers
+
+int n = 20;
+int sum = 0;
+for(int i =0; i<=20; i++){
+    sum = sum + (2*i + 1);
+}
+   System.out.println("sum of first 20 odd numbers");
+   System.out.println(sum);
+
+
+        }
+    }
+

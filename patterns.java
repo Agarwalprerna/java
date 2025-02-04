9c19075940f6407fc8e9adc8e9fe634004cde19a
PRERNA <147426811+Agarwalprerna@users.noreply.github.com>
2025-02-04 11:11:21 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,46 @@
+
+public class patterns {
+     public static void main(String args[]) { 
+
+        int n = 4;
+      for( int a =n; a >0; a-- ){
+    for(int j=0; j< a; j++){ 
+       System.out.print("*");
+    }
+System.out.println("\n");
+        
+      }   
+
+
+     /*   int n = 5;
+       for(int i = 1; i<= n; i++){
+              for(int j = 1;j <= i; j++){
+               System.out.print("*");
+              }
+              System.out.println();
+
+
+       } */
+
+
+     /*   int n = 5;
+       for(int i = 1; i <=n ; i++){
+        for(int j = 1; j <= n -i +1; j ++){
+         System.out.print(j);
+        }
+    System.out.println();
+       }  */
+            }
+             
+
+        
+            }
+        
+    
+
+
+
+     
+
+    
+

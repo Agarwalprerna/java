88049e000889f8b409858cabc44234901c4be82b
PRERNA <147426811+Agarwalprerna@users.noreply.github.com>
2025-02-04 11:02:06 +0530

    Add files via upload

---
+++ function_Cpractise.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+/*
+int power(int,int );            //function prototype only declaration
+int main(){
+	int base,exponent,result;                //arguments 
+	printf("enter the base value: ");
+	scanf("%d",&base);
+	
+	printf("enter the exponent of base: ");
+	scanf("%d",&exponent);
+	
+	result = power(base ,exponent);   //calling the function 
+	printf("%d ^%d = %d",base,exponent,result);
+	return 0;
+}	
+	//defining the task of funtion
+	int power(int base,int exponent){
+		if(exponent!=0)
+		return (base*power(base, exponent-1));
+		else
+		return 1;       //recursion stop condition
+	}
+
+*/
+
+int flip(int ,int);
+int main(){
+	int a,b,c,temp;
+	int N,m;
+	printf("how many digits do the number have: ");
+	scanf("%d",&m);
+	printf("enter the number");
+	scanf("%d",&N);
+	
+}
+
+int flip(int N,int m){
+
+}
+
